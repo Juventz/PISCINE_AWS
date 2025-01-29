b8e68b612870ab492f3747e74cc272f4dd76c281
juvaris <juvaris@My-Virtual-Machine.myguest.virtualbox.org>
2025-01-29 17:41:52 +0100

    update delete and import Depends

--- main.py
+++ main.py
@@ -1,6 +1,30 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Depends
 from pydantic import BaseModel
 from typing import List
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, Session
+
+# Connexion à la base de données
+SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db" # SQLite en local
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Déclaration de la base de données
+Base = declarative_base()
+
+
+# Modèle de la table User
+class UserDB(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    email = Column(String, unique=True, index=True)
+    age = Column(Integer)
+
+
+# Création de la table User
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
@@ -13,45 +37,56 @@ class User(BaseModel):
     age: int
 
 
-# Liste d'utilisateurs temporaires
-users: List[User] = []
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 # Route pour ajouter un utilisateur
 @app.post("/users/", response_model=User)
-def create_user(user: User):
+def create_user(user: User, db: Session = Depends(get_db)):
     # Verification email unique
-    if any(u.email == user.email for u in users):
+    db_user = db.query(UserDB).filter(UserDB.email == user.email).first()
+    if db_user:
         raise HTTPException(status_code=400, detail="Email already registered")
-    users.append(user)
-    return user
+    db_user = UserDB(**user.dict())
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
 
 
 # Route pour obtenir un utilisateur par son ID
 @app.get("/users/{user_id}", response_model=User)
-def get_user(user_id: int):
-    for user in users:
-        if user.id == user_id:
-            return user
-    raise HTTPException(status_code=404, detail="User not found")
+def get_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = db.query(UserDB).filter(UserDB.id == user_id).first()
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
 
 
 # Route pour mettre a jour un utilisateur par son ID
 @app.put("/users/{user_id}", response_model=User)
-def update_user(user_id: int, uptade_user: User):
-    for i, u in enumerate(users):
-        if u.id == user_id:
-            users[i] = update_user
-            return update_user
-    raise HTTPException(status_code=404, detail="User not found")
+def update_user(user_id: int, user: User, db: Session = Depends(get_db)):
+    db_user = db.query(UserDB).filter(UserDB.id == user_id).first()
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    for key, value in user.dict().items():
+        setattr(db_user, key, value)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
 
 
 # Route pour supprimer un utilisateur par son ID
 @app.delete("/users/{user_id}", response_model=User)
-def delete_user(user_id: int):
-    for i, user in enumerate(users):
-        if user.id == user_id:
-            delete_user = users.pop(i)
-            return {"message": "User deleted successfully",
-                    "deleted_user": delete_user}
-    raise HTTPException(status_code=404, detail="User not found")
+def delete_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = db.query(UserDB).filter(UserDB.id == user_id).first()
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    db.delete(db_user)
+    db.commit()
+    return db_user
