4043f4a2c584c1e37c0fd6f6b3a58d38172af38c
juvaris <juvaris@My-Virtual-Machine.myguest.virtualbox.org>
2025-01-30 18:53:57 +0100

    start making aws and sql link in python

--- database.py
+++ database.py
@@ -1,12 +1,24 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models import Base
+import pymysql
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+USERNAME = os.getenv("DB_USERNAME")
+PASSWORD = os.getenv("DB_PASSWORD")
+HOST = os.getenv("DB_HOST")
+PORT = os.getenv("DB_PORT")
+DATABASE = os.getenv("DB_NAME")
 
 # Connexion à la base de données
-SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{USERNAME}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}"
+
+# Creation de la session
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 # Creation de la table User
 Base.metadata.create_all(bind=engine)
- 
