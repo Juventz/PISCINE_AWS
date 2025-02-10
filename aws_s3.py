86d29dd10ff5e495b727b769b76f43d15a15136a
Juventz ARISTIL MATHURIN <jaristil@bess-f2r4s19.clusters.42paris.fr>
2025-02-10 16:32:33 +0100

    change in name of env variable

--- aws_s3.py
+++ aws_s3.py
@@ -4,14 +4,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+AWS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 AWS_BUCKET_NAME = os.getenv('AWS_BUCKET_NAME')
 
 s3_client = boto3.client(
     "s3",
-    aws_access_key_id=AWS_ACCESS_KEY_ID,
-    aws_secret_access_key=AWS_SECRET_ACCESS_KEY
+    aws_access_key_id=AWS_KEY_ID,
+    aws_secret_access_key=AWS_ACCESS_KEY
 )
 
 
